f5a4cbd48da4b416e1a70fd5947ef7169fbf110a
DTAPGAMING <43279945+dtapgaming@users.noreply.github.com>
2025-12-10 02:43:40 -0600

    Delete helpers directory

--- helpers/pathHelper.py
+++
@@ -1,7 +0,0 @@
-import bpy
-
-def getGamePath():
-    addon = bpy.context.preferences.addons.get("io_export_i3d_10_0_11")
-    if addon and hasattr(addon, "preferences") and getattr(addon.preferences, "game_install_path", ""):
-        return addon.preferences.game_install_path
-    return ""
